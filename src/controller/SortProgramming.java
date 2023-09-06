fe20c5531d3d1600cb01a138083543d0850e2e3d
ADMIN <ADMIN@bo>
2023-09-06 09:09:20 +0700

    csfsjsf

---
+++ src/controller/SortProgramming.java
@@ -0,0 +1,79 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controller;
+
+import Common.Algorithm;
+import Common.Library;
+import Model.Element;
+import View.Menu;
+
+/**
+ *
+ * @author ACER
+ */
+public class SortProgramming extends Menu<String> {
+
+    static String[] mc = {"Sort", "Exit"};
+
+    protected Library library;
+    protected Algorithm algorithm;
+    protected int[] array;
+    protected int size_array;
+
+    public SortProgramming(Element element) {
+        super("PROGRAMMING", mc);
+        library = new Library();
+        algorithm = new Algorithm();
+        size_array = element.getSize_array();
+        array = element.getArray();
+            }
+
+    public void execute(int n) {
+        switch (n) {
+            case 1:
+                sort();
+                break;
+            case 2:
+                System.exit(0);
+        }
+    }
+
+    public void sort() {
+        final String[] mcSort = {"Buddle Sort", "Quick Sort", "Exit"};
+        class SortMenu extends Menu<String> {
+
+            public SortMenu() {
+                super("Sort Option", mcSort);
+            }
+
+            @Override
+            public void execute(int n) {
+                System.out.println("Unsorted array: ");
+                library.display(array);
+                switch (n) {
+                    case 1:
+                        algorithm.buddleSort(array);
+                        System.out.println("\nSorted array by Buddle Sort: ");
+                        library.display(array);
+                        System.out.println("");
+                        break;
+                    case 2:
+                        algorithm.quickSort(array, 0, size_array - 1);
+                        System.out.println("\nSorted array by Quick Sort: ");
+                        library.display(array);
+                        System.out.println("");
+                        break;
+                    case 3:
+                        System.exit(0);
+                        break;
+                }
+            }
+        }
+        SortMenu sm = new SortMenu();
+        sm.run();
+    }
+
+
+}
---
+++ src/model/Element.java
@@ -0,0 +1,22 @@
+
+package Model;
+
+public class Element {
+    public int size_array;
+    public int[] array;
+    public int getSize_array() {
+        return size_array;
+    }
+
+    public void setSize_array(int size_array) {
+        this.size_array = size_array;
+    }
+
+    public int[] getArray() {
+        return array;
+    }
+
+    public void setArray(int[] array) {
+        this.array = array;
+    }
+}
